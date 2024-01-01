b0a7ef62fa9c5772851ddc8d7b7a4279196e3e11
m2barh <bongani@protogen.co.za>
2024-01-01 21:19:45 +0200

    coding exercise four commit

--- section_three/my-react-app/src/App.js
+++ section_three/my-react-app/src/App.js
@@ -1,38 +1,28 @@
 import React from 'react';
 
-export function MainGoal() {
-    return (
-        <p>
-        My main goal: Learn React from the ground outputting
-        </p>
-        );
+export const userData = {
+  firstName: 'Maximilian',
+  lastName: 'Schwarzmüller', 
+  title: 'Instructor',
+};
+
+export function User() {
+  return (
+    <div id="user" data-testid="user">
+      <h2>
+        {userData.firstName} {userData.lastName}
+      </h2>
+      <p>{userData.title}</p>
+    </div>
+  );
 }
 
 function App() {
   return (
     <div id="app">
-      <h1>Time to Practice!</h1>
-      <p>
-        Build a <code>&lt;MainGoal&gt;</code> component and insert it below this
-        text.
-      </p>
-      <p>
-        Your <code>&lt;MainGoal&gt;</code> component should simply output some
-        text that describes your main course goal (e.g., &quot;My main goal:
-        Learn React in great detail&quot;).
-      </p>
-      <p>
-        <strong>Important:</strong> You custom component must contain the text
-        &quot;My main goal:&quot;
-      </p>
-      <p>
-        <strong>Also important:</strong> For the automatic checks to succeed,
-        you <strong>must export</strong> your custom component function! Not as
-        a default but simply by adding the <code>export</code> keyword in front
-        of your function (e.g., <code>export function YOUR_COMPONENT_NAME</code>
-        ).
-      </p>
-      <MainGoal />
+      <h1>Time to Practice</h1>
+      <p>Welcome on board of this course! You got this 💪</p>
+      <User />
     </div>
   );
 }
