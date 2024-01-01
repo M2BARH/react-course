34e520e3615e366909f086bc02200d44eaa3b2c6
m2barh <bongani@protogen.co.za>
2024-01-01 23:11:52 +0200

    coding exercise thirteen commit

--- section_three/my-react-app/src/App.js
+++ section_three/my-react-app/src/App.js
@@ -1,17 +1,18 @@
-import React from 'react';
-
-import Todo from './components/todo_component/Todo'
-
-export const DUMMY_TODOS = [
-    'Learn React',
-    'Practice React',
-    'Profit!'
-];
-
-export default function App() {
-    return(
-        <ul>
-            {DUMMY_TODOS.map(todoItem => <Todo text={todoItem} />)}
-        </ul>
+function Summary({ text }) {
+  return (
+    <>
+        <h1>Summary</h1>
+        <p>{ text }</p>
+    </>
     );
 }
+
+function App() {
+  return (
+    <div id="app" data-testid="app">
+      <Summary text="Fragments help you avoid unnecessary HTML elements." />
+    </div>
+  );
+}
+
+export default App;
