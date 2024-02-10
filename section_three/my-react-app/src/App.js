02fe045d198cc73b69713810e7f242e35a48596b
m2barh <bongani@protogen.co.za>
2024-02-10 18:53:46 +0200

    coding exercise 17

--- section_three/my-react-app/src/App.js
+++ section_three/my-react-app/src/App.js
@@ -1,41 +1,25 @@
-import React from "react";
-import Review from "./components/review_component/Review";
+import React from 'react';
 
 function App() {
-  const [feedBack, setFeedBack] = React.useState();
-  const [name, setName] = React.useState();
-
-  function handleFeedBackChange(event) {
-    setFeedBack(event.target.value);
-  }
-
-  function handleNameChange(event) {
-    setName(event.target.value);
-  }
-
-  return (
-    <>
-      <section id="feedback">
-        <h2>Please share some feedback</h2>
-        <p>
-          <label>Your Feedback</label>
-          <textarea value={feedBack} onChange={handleFeedBackChange} />
-        </p>
-        <p>
-          <label>Your Name</label>
-          <input type="text" value={name} onChange={handleNameChange} />
-        </p>
-      </section>
-      <section id="draft">
-        <h2>Your feedback</h2>
-
-        <Review feedback={feedBack} student={name} />
-
-        <p>
-          <button>Save</button>
-        </p>
-      </section>
-    </>
+    
+    const [color, setColor] = React.useState('white');
+    
+    const handleClick = (newColor) => {
+        setColor(newColor);
+    };
+    
+    return (
+    <div id="app">
+      <h1 style={{color}}>CSS is great!</h1>
+      <menu>
+        <li>
+          <button onClick={() => handleClick('green')}>Yes</button>
+        </li>
+        <li>
+          <button onClick={() => handleClick('red')}>No</button>
+        </li>
+      </menu>
+    </div>
   );
 }
 
